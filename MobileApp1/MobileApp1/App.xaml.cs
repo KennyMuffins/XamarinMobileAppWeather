3518ff92a08e2a129a73b5f0df001cc8d82a07e9
Kenny Chua <kenny.chua@kydonlsg.com>
2020-12-03 07:48:48 +0800

    First commit

---
+++ MobileApp1/MobileApp1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MobileApp1.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CurrentWeatherPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
